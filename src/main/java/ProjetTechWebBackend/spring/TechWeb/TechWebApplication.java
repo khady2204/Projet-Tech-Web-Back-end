325c4d96540c9a2140a901908cbcf47d44195c0f
khady2204 <khady.kandji3@unchk.edu.sn>
2025-04-11 20:48:15 +0000

    Ajout Projet

---
+++ src/main/java/ProjetTechWebBackend/spring/TechWeb/TechWebApplication.java
@@ -0,0 +1,13 @@
+package ProjetTechWebBackend.spring.TechWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechWebApplication.class, args);
+	}
+
+}
